4aa4bd47a4142d521d5a2facfa8b3ed2a3723530
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-08-18 21:54:40 +0330

    Update comment model to apply the reviewer requested changes.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,12 +2,13 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  after_save :update_post_comments_counter
   after_create :update_post_comments_counter
   after_destroy :update_post_comments_counter
 
   private
 
   def update_post_comments_counter
-    post.update_comments_counter
+    post.increment!(:comments_counter)
   end
 end
