acc2bb8216bd01ac0b81be17ed9829a227eff47c
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-08-18 19:04:22 +0330

    Update the post model class to set dependent destroy on delete.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :comments
-  has_many :likes
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 end
