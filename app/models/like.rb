76afa9a3975a11cf70f0ac92ee35d072b100df07
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-08-18 20:33:06 +0330

    Update the like model to add update_like_counter method.

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,13 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  after_create :update_post_likes_counter
+  after_destroy :update_post_likes_counter
+
+  private
+
+  def update_post_likes_counter
+    post.update_posts_counter
+  end
 end
