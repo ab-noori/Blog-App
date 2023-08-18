7a6fbecd5905d0ced6f6db377837945c5590b089
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-08-18 20:31:26 +0330

    Update the comment model to add update_comment_counter  method.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,13 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  after_create :update_post_comments_counter
+  after_destroy :update_post_comments_counter
+
+  private
+
+  def update_post_comments_counter
+    post.update_comments_counter
+  end
 end
