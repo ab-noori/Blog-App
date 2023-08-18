c91aabe06453cb5c7aa5cf0e57f1bab276c838e2
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-08-18 20:26:47 +0330

    Update the post model to add recent_comments and update_posts  method.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  def update_posts_counter
+    update(posts_counter: comments.count + likes.count)
+  end
+
+  def recent_comments(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
+  end
 end
