ef877d21d7a0421b18617b57049d6c4223cddfa2
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-08-24 14:24:49 +0330

    Fix the liter errors.

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -24,23 +24,26 @@ RSpec.describe 'PostsController', type: :request do
   describe 'GET #show' do
     it 'returns a successful response' do
       user = User.create(id: 1, name: 'ali', posts_counter: 0)
-      post = Post.create(id: 1, author_id: user.id, title: 'Test Post 1', text: 'test content', comments_counter: 0, likes_counter: 0)
+      post = Post.create(id: 1, author_id: user.id, title: 'Test Post 1', text: 'test content', comments_counter: 0,
+                         likes_counter: 0)
       get user_post_path(user, post)
       expect(response).to have_http_status(:success)
     end
-  
+
     it 'renders the show template' do
       user = User.create(id: 1, name: 'ali', posts_counter: 0)
-      post = Post.create(id: 1, author_id: user.id, title: 'Test Post 1', text: 'test content', comments_counter: 0, likes_counter: 0)
+      post = Post.create(id: 1, author_id: user.id, title: 'Test Post 1', text: 'test content', comments_counter: 0,
+                         likes_counter: 0)
       get user_post_path(user, post)
       expect(response).to render_template(:show)
     end
-  
+
     it 'includes correct placeholder text in the response body' do
       user = User.create(id: 1, name: 'ali', posts_counter: 0)
-      post = Post.create(id: 1, author_id: user.id, title: 'Test Post 1', text: 'test content', comments_counter: 0, likes_counter: 0)
+      post = Post.create(id: 1, author_id: user.id, title: 'Test Post 1', text: 'test content', comments_counter: 0,
+                         likes_counter: 0)
       get user_post_path(user, post)
       expect(response.body).to include('Post Details')
     end
-  end  
+  end
 end
--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -25,17 +25,17 @@ RSpec.describe 'UsersController', type: :request do
       get user_path(user)
       expect(response).to have_http_status(:success)
     end
-  
+
     it 'renders the show template' do
       user = User.create(id: 2, name: 'Test User 2')
       get user_path(user)
       expect(response).to render_template(:show)
     end
-  
+
     it 'includes correct placeholder text in the response body' do
       user = User.create(id: 3, name: 'Test User 3')
       get user_path(user)
       expect(response.body).to include('User Profile')
     end
-  end  
+  end
 end
