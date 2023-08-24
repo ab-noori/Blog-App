e6a33ae8dda7062d42f78fa701036a75d9f6c585
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-08-24 14:19:56 +0330

    Add testing cases for the post controller.

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -3,37 +3,43 @@ require 'rails_helper'
 RSpec.describe 'PostsController', type: :request do
   describe 'GET #index' do
     it 'returns a successful response' do
-      get posts_path
+      user = User.create(id: 1)
+      get user_posts_path(user)
       expect(response).to have_http_status(:success)
     end
 
     it 'renders the index template' do
-      get posts_path
+      user = User.create(id: 1)
+      get user_posts_path(user)
       expect(response).to render_template(:index)
     end
 
     it 'includes correct placeholder text in the response body' do
-      get posts_path
+      user = User.create(id: 1)
+      get user_posts_path(user)
       expect(response.body).to include('Posts for a User')
     end
   end
 
   describe 'GET #show' do
     it 'returns a successful response' do
-      post = Post.create(id: 1, title: 'Test Post 1')
-      get post_path(post)
+      user = User.create(id: 1, name: 'ali', posts_counter: 0)
+      post = Post.create(id: 1, author_id: user.id, title: 'Test Post 1', text: 'test content', comments_counter: 0, likes_counter: 0)
+      get user_post_path(user, post)
       expect(response).to have_http_status(:success)
     end
   
     it 'renders the show template' do
-      post = Post.create(id: 2, title: 'Test Post 2')
-      get post_path(post)
+      user = User.create(id: 1, name: 'ali', posts_counter: 0)
+      post = Post.create(id: 1, author_id: user.id, title: 'Test Post 1', text: 'test content', comments_counter: 0, likes_counter: 0)
+      get user_post_path(user, post)
       expect(response).to render_template(:show)
     end
   
     it 'includes correct placeholder text in the response body' do
-      post = Post.create(id: 3, title: 'Test Post 3')
-      get post_path(post)
+      user = User.create(id: 1, name: 'ali', posts_counter: 0)
+      post = Post.create(id: 1, author_id: user.id, title: 'Test Post 1', text: 'test content', comments_counter: 0, likes_counter: 0)
+      get user_post_path(user, post)
       expect(response.body).to include('Post Details')
     end
   end  
