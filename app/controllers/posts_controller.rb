dc23afc3d78c620539512bd308a13a0571589c82
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-08-25 23:22:28 +0330

    Modifye the posts controller to list each user post and post details.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,11 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.order(created_at: :asc)
+  end
 
-  def show; end
+  def show
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:id])
+  end
 end
