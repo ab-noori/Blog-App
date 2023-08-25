5777b7705f5ab7ce59f3f04bbd1377304a5fadfe
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-08-25 23:21:15 +0330

    Modifye the user controller to list users and user details.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
-  def index; end
+  def index
+    @users = User.all.order(id: :asc)
+  end
 
-  def show; end
+  def show
+    @user = User.find(params[:id])
+    @recent_posts = @user.recent_posts
+  end
 end
