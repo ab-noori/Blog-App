8a458aa1b24e35e7719f4057882f267fd2828a4d
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-08-26 18:13:22 +0330

    Create the comments controller to handle the creation of new post.

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.author = @current_user
+
+    if @comment.save
+        redirect_to user_post_path(user_id: @current_user.id, id: @post.id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
