c6086fc27b8ab7f93decb6da72d66099927e2a08
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-08-26 18:09:07 +0330

    Modify the post controller to add method needed for creatint a new post.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,24 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = @current_user.posts.build(post_params)
+
+    if @post.save 
+      redirect_to user_post_path(user_id: @current_user.id, id: @post.id)
+    else 
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
