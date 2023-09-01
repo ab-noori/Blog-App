243a5bdc2450a0495a8d60ff8323ee5da55708d4
kifle23 <kiflekifle23@gmail.com>
2023-09-01 20:49:10 +0300

    Update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,11 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
-    helper_method :current_user
-  
-    protected
-  
-    # def update_allowed_parameters
-    #   devise_parameter_sanitizer.permit(:sign_up) do |u|
-    #     u.permit(:name, :email, :password, :password_confirmation)
-    #   end
-    # end
+    before_action :authenticate_user!, :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :email, :password, :password_confirmation)
+    end
+  end
 end
