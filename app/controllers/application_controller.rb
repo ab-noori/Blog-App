c8f634e6981b23e59f2fee9a28be2110eeb05d17
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-09-01 19:31:59 +0330

    Remove the current user method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
 class ApplicationController < ActionController::Base
-  before_action :set_current_user
-
-  private
-
-  def set_current_user
-    @current_user = User.first
-  end
+    before_action :authenticate_user!
+    helper_method :current_user
+  
+    protected
+  
+    # def update_allowed_parameters
+    #   devise_parameter_sanitizer.permit(:sign_up) do |u|
+    #     u.permit(:name, :email, :password, :password_confirmation)
+    #   end
+    # end
 end
