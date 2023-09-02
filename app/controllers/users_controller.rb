6a077fa947dbc4ce88cd14827686d37db2f38618
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-09-02 14:44:13 +0330

    Modify user controller to set current user by devise.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,17 @@ class UsersController < DeviseController
   before_action :authenticate_user!
   
   def index
-    @users = User.all.order(id: :asc)
+    if current_user 
+      @users = User.all.order(id: :asc)
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   def show
     if params[:id] == 'sign_out'
       sign_out current_user
-      redirect_to users_path
+      redirect_to new_user_session_path
     else
       @user = User.includes(:posts).find(params[:id])
     end
